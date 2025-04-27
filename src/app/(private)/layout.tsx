bb8cdf0d2bbd5546e97ae0cb04a5d4f6b95ad79e
Your Name <you@example.com>
2025-04-28 08:46:57 +0900

    add crud

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -7,7 +7,9 @@ export default function PrivateLayout({
   return (
     <>
       <PrivateHeader />
+      <div className="container mx-auto px-4 py-8">
       {children}
+      </div>
     </>
   )
 }
