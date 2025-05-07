8fb50147e1d183cac8b25354da21556376bcb33e
Your Name <you@example.com>
2025-05-07 20:26:16 +0900

    re generate

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,10 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // ← build時もeslintエラー無視
   },
+  webpack: (config) => {
+    config.externals = [...config.externals, '@prisma/client'];
+    return config;
+  },
 };
 
 export default nextConfig;
